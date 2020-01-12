49b833ee3ca882285c2c93ea4c844e671048da68
Lxk3y <47719567+Lxk3y@users.noreply.github.com>
2020-01-12 15:42:47 +0000

    Fixed Depreciations

--- client.lua
+++ client.lua
@@ -44,28 +44,61 @@ end)
 
 RegisterNetEvent("DisplayMessage")
 AddEventHandler("DisplayMessage", function(source)
-	TriggerEvent("chatMessage", "^1Uh Oh! It appears you have the following ^3" ..#colorsFound.. " ^1colors in your name:")
+	TriggerEvent("chat:addMessage", {
+		args = { "^1Uh Oh! It appears you have the following ^3" ..#colorsFound.. " ^1colors in your name:" }
+	})
 	for key, color in pairs(colorsFound) do
 		if color == "~r~" then
-			TriggerEvent("chatMessage", color, { 255, 68, 68 })
+			TriggerEvent("chat:addMessage", {
+				color = { 255, 68, 68 },
+				args = { color }
+			})
 		elseif color == "~b~" then
-			TriggerEvent("chatMessage", color, { 58, 135, 212 })
+			TriggerEvent("chat:addMessage", {
+				color = { 58, 135, 212 },
+				args = { color }
+			})
 		elseif color == "~g~" then
-			TriggerEvent("chatMessage", color, { 65, 174, 65 })
+			TriggerEvent("chat:addMessage", {
+				color = { 65, 174, 65 },
+				args = { color }
+			})
 		elseif color == "~y~" then
-			TriggerEvent("chatMessage", color, { 238, 238, 26 })
+			TriggerEvent("chat:addMessage", {
+				color = { 238, 238, 26 },
+				args = { color }
+			})
 		elseif color == "~o~" then
-			TriggerEvent("chatMessage", color, { 238, 167, 26 })
+			TriggerEvent("chat:addMessage", {
+				color = { 238, 167, 26 },
+				args = { color }
+			})
 		elseif color == "~p~" then
-			TriggerEvent("chatMessage", color, { 179, 49, 199 })
+			TriggerEvent("chat:addMessage", {
+				color = { 179, 49, 199 },
+				args = { color }
+			})
 		elseif color == "~c~" then
-			TriggerEvent("chatMessage", color, { 156, 155, 156 })
+			TriggerEvent("chat:addMessage", {
+				color = { 156, 155, 156 },
+				args = { color }
+			})
 		elseif color == "~m~" then
-			TriggerEvent("chatMessage", color, { 85, 84, 85 })
+			TriggerEvent("chat:addMessage", {
+				color = { 85, 84, 85 },
+				args = { color }
+			})
 		elseif color == "~u~" then
-			TriggerEvent("chatMessage", color, { 0, 0, 0 })
+			TriggerEvent("chat:addMessage", {
+				color = { 0, 0, 0 },
+				args = { color }
+			})
 		end
 	end
-	TriggerEvent("chatMessage", "^1We dont allow colored names on our server!")
-	TriggerEvent("chatMessage", "^3We kindly ask if you would remove them before playing with us!")
+	TriggerEvent("chat:addMessage", {
+		args = { "^1We dont allow colored names on our server!" }
+	})
+	TriggerEvent("chat:addMessage", {
+		args = { "^3We kindly ask if you would remove them before playing with us!" }
+	})
 end)
